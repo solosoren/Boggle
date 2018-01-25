82a545f9e8149a3c5fb877a2805eb32ca3ef05a6
kunaalkumar <kunaal@ashiana.com>
2018-01-25 11:11:32 -0700

    Corrected namespace name to original.

--- Spreadsheet/Formula/Formula.cs
+++ Spreadsheet/Formula/Formula.cs
@@ -7,7 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
-namespace Formula
+namespace Formulas
 {
     /// <summary>
     /// Represents formulas written in standard infix notation using standard precedence
