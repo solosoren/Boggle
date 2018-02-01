4e563742c0c9e2c8cebd9e288143cbfab9a95dec
kunaalkumar <kunaal@ashiana.com>
2018-02-01 12:32:44 -0700

    Removes node from ist if it has no dependent or dependees.

---
+++ Spreadsheet/DependencyGraphTestCases/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DependencyGraphTestCases
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
