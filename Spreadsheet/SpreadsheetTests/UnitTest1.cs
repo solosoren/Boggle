7dc280b6c91c27476f4f67d78fb1ec23196aaa73
kunaalkumar <kunaal@ashiana.com>
2018-02-14 22:19:26 -0700

    Added Unit Test File

---
+++ Spreadsheet/SpreadsheetTests/UnitTest1.cs
@@ -0,0 +1,21 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SS;
+
+namespace SpreadsheetTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Spreadsheet spreadsheet = new Spreadsheet();
+
+            foreach (var VARIABLE in spreadsheet.GetNamesOfAllNonemptyCells())
+            {
+                Console.WriteLine(VARIABLE);
+            }
+        }
+    }
+}
