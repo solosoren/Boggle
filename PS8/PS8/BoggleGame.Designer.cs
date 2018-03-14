166c53feda5270247fc2a33a7c5fcb941894fd45
Kunaal Kumar <Kunaal@ashiana.com>
2018-03-14 00:28:00 -0600

    - Add Boggle Game ui. (I'm sure there is a better way to do this but this should work for now.)

---
+++ PS8/PS8/BoggleGame.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace PS8
+{
+    partial class BoggleGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.player1Name = new System.Windows.Forms.Label();
+            this.player2Name = new System.Windows.Forms.Label();
+            this.player1Score = new System.Windows.Forms.Label();
+            this.player2Score = new System.Windows.Forms.Label();
+            this.wordTextBox = new System.Windows.Forms.TextBox();
+            this.col1row1 = new System.Windows.Forms.Label();
+            this.col2row1 = new System.Windows.Forms.Label();
+            this.col3row1 = new System.Windows.Forms.Label();
+            this.col4row1 = new System.Windows.Forms.Label();
+            this.col1row2 = new System.Windows.Forms.Label();
+            this.col2row2 = new System.Windows.Forms.Label();
+            this.col3row2 = new System.Windows.Forms.Label();
+            this.col4row2 = new System.Windows.Forms.Label();
+            this.col1row3 = new System.Windows.Forms.Label();
+            this.col2row3 = new System.Windows.Forms.Label();
+            this.col3row3 = new System.Windows.Forms.Label();
+            this.col4row3 = new System.Windows.Forms.Label();
+            this.col1row4 = new System.Windows.Forms.Label();
+            this.col2row4 = new System.Windows.Forms.Label();
+            this.col3row4 = new System.Windows.Forms.Label();
+            this.col4row4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // player1Name
+            // 
+            this.player1Name.AutoSize = true;
+            this.player1Name.Location = new System.Drawing.Point(37, 33);
+            this.player1Name.Name = "player1Name";
+            this.player1Name.Size = new System.Drawing.Size(69, 13);
+            this.player1Name.TabIndex = 0;
+            this.player1Name.Text = "player1Name";
+            // 
+            // player2Name
+            // 
+            this.player2Name.AutoSize = true;
+            this.player2Name.Location = new System.Drawing.Point(373, 33);
+            this.player2Name.Name = "player2Name";
+            this.player2Name.Size = new System.Drawing.Size(69, 13);
+            this.player2Name.TabIndex = 1;
+            this.player2Name.Text = "player2Name";
+            // 
+            // player1Score
+            // 
+            this.player1Score.AutoSize = true;
+            this.player1Score.Location = new System.Drawing.Point(37, 67);
+            this.player1Score.Name = "player1Score";
+            this.player1Score.Size = new System.Drawing.Size(69, 13);
+            this.player1Score.TabIndex = 2;
+            this.player1Score.Text = "player1Score";
+            // 
+            // player2Score
+            // 
+            this.player2Score.AutoSize = true;
+            this.player2Score.Location = new System.Drawing.Point(373, 67);
+            this.player2Score.Name = "player2Score";
+            this.player2Score.Size = new System.Drawing.Size(69, 13);
+            this.player2Score.TabIndex = 3;
+            this.player2Score.Text = "player2Score";
+            // 
+            // wordTextBox
+            // 
+            this.wordTextBox.Location = new System.Drawing.Point(179, 299);
+            this.wordTextBox.Name = "wordTextBox";
+            this.wordTextBox.Size = new System.Drawing.Size(108, 20);
+            this.wordTextBox.TabIndex = 4;
+            // 
+            // col1row1
+            // 
+            this.col1row1.AutoSize = true;
+            this.col1row1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.col1row1.Location = new System.Drawing.Point(134, 94);
+            this.col1row1.Name = "col1row1";
+            this.col1row1.Size = new System.Drawing.Size(47, 46);
+            this.col1row1.TabIndex = 16;
+            this.col1row1.Text = "A";
+            // 
+            // col2row1
+            // 
+            this.col2row1.AutoSize = true;
+            this.col2row1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.col2row1.Location = new System.Drawing.Point(187, 94);
+            this.col2row1.Name = "col2row1";
+            this.col2row1.Size = new System.Drawing.Size(47, 46);
+            this.col2row1.TabIndex = 17;
+            this.col2row1.Text = "B";
+            // 
+            // col3row1
+            // 
+            this.col3row1.AutoSize = true;
+            this.col3row1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.col3row1.Location = new System.Drawing.Point(236, 94);
+            this.col3row1.Name = "col3row1";
+            this.col3row1.Size = new System.Drawing.Size(49, 46);
+            this.col3row1.TabIndex = 18;
+            this.col3row1.Text = "C";
+            // 
+            // col4row1
+            // 
+            this.col4row1.AutoSize = true;
+            this.col4row1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.col4row1.Location = new System.Drawing.Point(291, 94);
+            this.col4row1.Name = "col4row1";
+            this.col4row1.Size = new System.Drawing.Size(49, 46);
+            this.col4row1.TabIndex = 19;
+            this.col4row1.Text = "D";
+            // 
+            // col1row2
+            // 
+            this.col1row2.AutoSize = true;
+            this.col1row2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.col1row2.Location = new System.Drawing.Point(134, 144);
+            this.col1row2.Name = "col1row2";
+            this.col1row2.Size = new System.Drawing.Size(47, 46);
+            this.col1row2.TabIndex = 20;
+            this.col1row2.Text = "E";
+            // 
+            // col2row2
+            // 
+            this.col2row2.AutoSize = true;
+            this.col2row2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.col2row2.Location = new System.Drawing.Point(187, 144);
+            this.col2row2.Name = "col2row2";
+            this.col2row2.Size = new System.Drawing.Size(44, 46);
+            this.col2row2.TabIndex = 21;
+            this.col2row2.Text = "F";
+            // 
+            // col3row2
+            // 
+            this.col3row2.AutoSize = true;
+            this.col3row2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.col3row2.Location = new System.Drawing.Point(236, 144);
+            this.col3row2.Name = "col3row2";
+            this.col3row2.Size = new System.Drawing.Size(51, 46);
+            this.col3row2.TabIndex = 22;
+            this.col3row2.Text = "G";
+            // 
+            // col4row2
+            // 
+            this.col4row2.AutoSize = true;
+            this.col4row2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.col4row2.Location = new System.Drawing.Point(291, 144);
+            this.col4row2.Name = "col4row2";
+            this.col4row2.Size = new System.Drawing.Size(49, 46);
+            this.col4row2.TabIndex = 23;
+            this.col4row2.Text = "H";
+            // 
+            // col1row3
+            // 
+            this.col1row3.AutoSize = true;
+            this.col1row3.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.col1row3.Location = new System.Drawing.Point(134, 190);
+            this.col1row3.Name = "col1row3";
+            this.col1row3.Size = new System.Drawing.Size(30, 46);
+            this.col1row3.TabIndex = 24;
+            this.col1row3.Text = "I";
+            // 
+            // col2row3
+            // 
+            this.col2row3.AutoSize = true;
+            this.col2row3.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.col2row3.Location = new System.Drawing.Point(187, 190);
+            this.col2row3.Name = "col2row3";
+            this.col2row3.Size = new System.Drawing.Size(40, 46);
+            this.col2row3.TabIndex = 25;
+            this.col2row3.Text = "J";
+            // 
+            // col3row3
+            // 
+            this.col3row3.AutoSize = true;
+            this.col3row3.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.col3row3.Location = new System.Drawing.Point(236, 190);
+            this.col3row3.Name = "col3row3";
+            this.col3row3.Size = new System.Drawing.Size(46, 46);
+            this.col3row3.TabIndex = 26;
+            this.col3row3.Text = "K";
+            // 
+            // col4row3
+            // 
+            this.col4row3.AutoSize = true;
+            this.col4row3.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.col4row3.Location = new System.Drawing.Point(291, 190);
+            this.col4row3.Name = "col4row3";
+            this.col4row3.Size = new System.Drawing.Size(42, 46);
+            this.col4row3.TabIndex = 27;
+            this.col4row3.Text = "L";
+            // 
+            // col1row4
+            // 
+            this.col1row4.AutoSize = true;
+            this.col1row4.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.col1row4.Location = new System.Drawing.Point(134, 236);
+            this.col1row4.Name = "col1row4";
+            this.col1row4.Size = new System.Drawing.Size(53, 46);
+            this.col1row4.TabIndex = 28;
+            this.col1row4.Text = "M";
+            // 
+            // col2row4
+            // 
+            this.col2row4.AutoSize = true;
+            this.col2row4.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.col2row4.Location = new System.Drawing.Point(187, 236);
+            this.col2row4.Name = "col2row4";
+            this.col2row4.Size = new System.Drawing.Size(49, 46);
+            this.col2row4.TabIndex = 29;
+            this.col2row4.Text = "N";
+            // 
+            // col3row4
+            // 
+            this.col3row4.AutoSize = true;
+            this.col3row4.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.col3row4.Location = new System.Drawing.Point(236, 236);
+            this.col3row4.Name = "col3row4";
+            this.col3row4.Size = new System.Drawing.Size(51, 46);
+            this.col3row4.TabIndex = 30;
+            this.col3row4.Text = "O";
+            // 
+            // col4row4
+            // 
+            this.col4row4.AutoSize = true;
+            this.col4row4.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.col4row4.Location = new System.Drawing.Point(291, 236);
+            this.col4row4.Name = "col4row4";
+            this.col4row4.Size = new System.Drawing.Size(47, 46);
+            this.col4row4.TabIndex = 31;
+            this.col4row4.Text = "P";
+            // 
+            // BoggleGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 361);
+            this.Controls.Add(this.col1row1);
+            this.Controls.Add(this.col2row1);
+            this.Controls.Add(this.col3row1);
+            this.Controls.Add(this.col4row1);
+            this.Controls.Add(this.col1row2);
+            this.Controls.Add(this.col2row2);
+            this.Controls.Add(this.col3row2);
+            this.Controls.Add(this.col4row2);
+            this.Controls.Add(this.col1row3);
+            this.Controls.Add(this.col2row3);
+            this.Controls.Add(this.col3row3);
+            this.Controls.Add(this.col4row3);
+            this.Controls.Add(this.col1row4);
+            this.Controls.Add(this.col2row4);
+            this.Controls.Add(this.col3row4);
+            this.Controls.Add(this.col4row4);
+            this.Controls.Add(this.wordTextBox);
+            this.Controls.Add(this.player2Score);
+            this.Controls.Add(this.player1Score);
+            this.Controls.Add(this.player2Name);
+            this.Controls.Add(this.player1Name);
+            this.MaximumSize = new System.Drawing.Size(500, 400);
+            this.MinimumSize = new System.Drawing.Size(500, 400);
+            this.Name = "BoggleGame";
+            this.Text = "Boggle Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label player1Name;
+        private System.Windows.Forms.Label player2Name;
+        private System.Windows.Forms.Label player1Score;
+        private System.Windows.Forms.Label player2Score;
+        private System.Windows.Forms.TextBox wordTextBox;
+        private System.Windows.Forms.Label col1row1;
+        private System.Windows.Forms.Label col2row1;
+        private System.Windows.Forms.Label col3row1;
+        private System.Windows.Forms.Label col4row1;
+        private System.Windows.Forms.Label col1row2;
+        private System.Windows.Forms.Label col2row2;
+        private System.Windows.Forms.Label col3row2;
+        private System.Windows.Forms.Label col4row2;
+        private System.Windows.Forms.Label col1row3;
+        private System.Windows.Forms.Label col2row3;
+        private System.Windows.Forms.Label col3row3;
+        private System.Windows.Forms.Label col4row3;
+        private System.Windows.Forms.Label col1row4;
+        private System.Windows.Forms.Label col2row4;
+        private System.Windows.Forms.Label col3row4;
+        private System.Windows.Forms.Label col4row4;
+    }
+}
